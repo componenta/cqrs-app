e11920c86586e8740b583a318fdd6ff7850b401f
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-02 00:10:32 +0200

    Restore CQRS handler map contract

--- src/Compile/CommandHandlerMapCompiler.php
+++ src/Compile/CommandHandlerMapCompiler.php
@@ -8,7 +8,6 @@ use Componenta\ClassFinder\Compile\CompileResult;
 use Componenta\ClassFinder\Compile\ListenerCompilerInterface;
 use Componenta\CQRS\App\Command\Locator\AttributeCommandHandlerLocator;
 use Componenta\CQRS\ConfigKey;
-use ReflectionMethod;
 
 /**
  * Serialises {@see AttributeCommandHandlerLocator}'s populated map into the
@@ -24,28 +23,6 @@ final class CommandHandlerMapCompiler implements ListenerCompilerInterface
     public function compile(object $listener, string $cacheDir): CompileResult
     {
         /** @var AttributeCommandHandlerLocator $listener */
-        return CompileResult::config(
-            ConfigKey::COMMAND_HANDLER_MAP,
-            $this->withInvocationMode($listener->toArray()),
-        );
-    }
-
-    /**
-     * @param array<string, array{0: class-string, 1: string}> $map
-     * @return array<string, array{0: class-string, 1: string, 2: bool}>
-     */
-    private function withInvocationMode(array $map): array
-    {
-        foreach ($map as $message => [$handler, $method]) {
-            $reflection = new ReflectionMethod($handler, $method);
-            $parameters = $reflection->getParameters();
-            $map[$message] = [
-                $handler,
-                $method,
-                count($parameters) === 1 && !$parameters[0]->isVariadic(),
-            ];
-        }
-
-        return $map;
+        return CompileResult::config(ConfigKey::COMMAND_HANDLER_MAP, $listener->toArray());
     }
 }
--- src/Compile/QueryHandlerMapCompiler.php
+++ src/Compile/QueryHandlerMapCompiler.php
@@ -8,7 +8,6 @@ use Componenta\ClassFinder\Compile\CompileResult;
 use Componenta\ClassFinder\Compile\ListenerCompilerInterface;
 use Componenta\CQRS\ConfigKey;
 use Componenta\CQRS\App\Query\Locator\AttributeQueryHandlerLocator;
-use ReflectionMethod;
 
 /**
  * Serialises {@see AttributeQueryHandlerLocator}'s populated map into the
@@ -25,28 +24,6 @@ final class QueryHandlerMapCompiler implements ListenerCompilerInterface
     public function compile(object $listener, string $cacheDir): CompileResult
     {
         /** @var AttributeQueryHandlerLocator $listener */
-        return CompileResult::config(
-            ConfigKey::QUERY_HANDLER_MAP,
-            $this->withInvocationMode($listener->toArray()),
-        );
-    }
-
-    /**
-     * @param array<string, array{0: class-string, 1: string}> $map
-     * @return array<string, array{0: class-string, 1: string, 2: bool}>
-     */
-    private function withInvocationMode(array $map): array
-    {
-        foreach ($map as $message => [$handler, $method]) {
-            $reflection = new ReflectionMethod($handler, $method);
-            $parameters = $reflection->getParameters();
-            $map[$message] = [
-                $handler,
-                $method,
-                count($parameters) === 1 && !$parameters[0]->isVariadic(),
-            ];
-        }
-
-        return $map;
+        return CompileResult::config(ConfigKey::QUERY_HANDLER_MAP, $listener->toArray());
     }
 }
