81e92ee0a8ffadff46478019bc207d0079947af7
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-01 20:18:28 +0200

    Compile direct CQRS handler maps

--- src/Compile/QueryHandlerMapCompiler.php
+++ src/Compile/QueryHandlerMapCompiler.php
@@ -8,6 +8,7 @@ use Componenta\ClassFinder\Compile\CompileResult;
 use Componenta\ClassFinder\Compile\ListenerCompilerInterface;
 use Componenta\CQRS\ConfigKey;
 use Componenta\CQRS\App\Query\Locator\AttributeQueryHandlerLocator;
+use ReflectionMethod;
 
 /**
  * Serialises {@see AttributeQueryHandlerLocator}'s populated map into the
@@ -24,6 +25,28 @@ final class QueryHandlerMapCompiler implements ListenerCompilerInterface
     public function compile(object $listener, string $cacheDir): CompileResult
     {
         /** @var AttributeQueryHandlerLocator $listener */
-        return CompileResult::config(ConfigKey::QUERY_HANDLER_MAP, $listener->toArray());
+        return CompileResult::config(
+            ConfigKey::QUERY_HANDLER_MAP,
+            $this->withInvocationMode($listener->toArray()),
+        );
+    }
+
+    /**
+     * @param array<string, array{0: class-string, 1: string}> $map
+     * @return array<string, array{0: class-string, 1: string, 2: bool}>
+     */
+    private function withInvocationMode(array $map): array
+    {
+        foreach ($map as $message => [$handler, $method]) {
+            $reflection = new ReflectionMethod($handler, $method);
+            $parameters = $reflection->getParameters();
+            $map[$message] = [
+                $handler,
+                $method,
+                count($parameters) === 1 && !$parameters[0]->isVariadic(),
+            ];
+        }
+
+        return $map;
     }
 }
