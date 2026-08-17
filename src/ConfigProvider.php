3bcf0dce02449aba4ee2f4c2233f161f60dbc7c2
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-17 10:57:35 +0200

    Bind one effective CQRS map provider

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -8,18 +8,12 @@ use Componenta\App\ConfigKey as AppConfigKey;
 use Componenta\ClassFinder\Compile\ConfigKey as CompileConfigKey;
 use Componenta\ClassFinder\ConfigKey as ClassFinderConfigKey;
 use Componenta\Config\ConfigProvider as BaseConfigProvider;
-use Componenta\CQRS\App\Command\Factory\CommandHandlerLocatorFactory;
-use Componenta\CQRS\App\Command\Factory\CommandListenersLocatorFactory;
 use Componenta\CQRS\App\Compile\CqrsMapCompiler;
 use Componenta\CQRS\App\Compile\Factory\CqrsMapCompilerFactory;
 use Componenta\CQRS\App\Discovery\CqrsDiscoveryIndex;
 use Componenta\CQRS\App\Discovery\Factory\CqrsDiscoveryIndexFactory;
-use Componenta\CQRS\App\Map\ApplicationCqrsMapProvider;
 use Componenta\CQRS\App\Map\Factory\ApplicationCqrsMapProviderFactory;
-use Componenta\CQRS\App\Query\Factory\QueryHandlerLocatorFactory;
-use Componenta\CQRS\Command\Locator\CommandHandlerLocatorInterface;
-use Componenta\CQRS\Command\Locator\CommandListenersLocatorInterface;
-use Componenta\CQRS\Query\Locator\QueryHandlerLocatorInterface;
+use Componenta\CQRS\Map\CqrsMapProviderInterface;
 
 final class ConfigProvider extends BaseConfigProvider
 {
@@ -28,10 +22,7 @@ final class ConfigProvider extends BaseConfigProvider
         return [
             CqrsMapCompiler::class => CqrsMapCompilerFactory::class,
             CqrsDiscoveryIndex::class => CqrsDiscoveryIndexFactory::class,
-            ApplicationCqrsMapProvider::class => ApplicationCqrsMapProviderFactory::class,
-            QueryHandlerLocatorInterface::class => QueryHandlerLocatorFactory::class,
-            CommandHandlerLocatorInterface::class => CommandHandlerLocatorFactory::class,
-            CommandListenersLocatorInterface::class => CommandListenersLocatorFactory::class,
+            CqrsMapProviderInterface::class => ApplicationCqrsMapProviderFactory::class,
         ];
     }
 
@@ -47,4 +38,3 @@ final class ConfigProvider extends BaseConfigProvider
         ];
     }
 }
-
